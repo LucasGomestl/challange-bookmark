8cea6a85962ae4635c1d557ad553d15014939530
Lucas Gomes <lucasgomes_tl@hotmail.com>
2019-10-28 22:57:25 -0400

    Add animations to tabs and turns it reponsive

--- js/features.js
+++ js/features.js
@@ -1,17 +1,40 @@
 $(function(){
-    const underline = $('.underline');
+    const navUnderline = $('.navUnderline');
     const button0 = $('.tabs-component>li[data-key=0]');
     const button1 = $('.tabs-component>li[data-key=1]');
     const button2 = $('.tabs-component>li[data-key=2]');
 
     button0.click(function(){
-        underline.css('left', '0%')
-    })
+        
+        navUnderline.css('left', '0%');
+    
+        $(button0.addClass('-active'))
+        $(button1.removeClass('-active'))
+        $(button2.removeClass('-active'))
+    
+    });
     button1.click(function(){
-        underline.css('left', '33.3%')
-    })
+        navUnderline.css('left', '33.33%');
+        
+        $(button1.addClass('-active'))
+        $(button2.removeClass('-active'))
+        $(button0.removeClass('-active'))
+        
+       
+    });
     button2.click(function(){
-        underline.css('left', '66.66%')
-    })
+        
+        navUnderline.css('left', '66.66%');
+    
+        $(button2.addClass('-active'))
+        $(button0.removeClass('-active'))
+        $(button1.removeClass('-active'))
+    
+    });
 
-})
+    //When the page is load, verify if the window width is less than 1100px
+    //if it is, the first button will be active
+    if(window.innerWidth < 1100){
+        $(button0.addClass('-active'))
+    }
+});
